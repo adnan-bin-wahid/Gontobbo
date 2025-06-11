11fe49d44af73eb41b4515fe3c9dfdae3262cc87
adnan-bin-wahid <adnan.iit.du@gmail.com>
2025-06-12 00:15:04 +0600

    20 minutes

---
+++ Backend/db/db.js
@@ -0,0 +1,15 @@
+const  mongoose = require('mongoose');
+
+function connectToDb() {
+    console.log('Connected to MongoDB');
+    console.log('mongo uri', process.env.MONGO_URI);
+  mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err);
+  });
+}
+
+module.exports = connectToDb;
