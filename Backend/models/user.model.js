c157e59665283cb4ac1a444051bae5b0794c0607
adnan-bin-wahid <adnan.iit.du@gmail.com>
2025-06-28 02:54:18 +0600

    user module done

---
+++ Backend/models/user.model.js
@@ -0,0 +1,61 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const jwt_token = process.env.JWT_SECRET;
+console.log("jwt token", jwt_token); 
+
+const userSchema = new mongoose.Schema({
+    fullname:{
+        firstname:{
+            type: String,
+            required: true,
+            minlength:[3,'First name must be at least 3 characters long'], 
+        },
+        lastname:{
+            type: String,
+            minlength:[3,'Last name must be at least 3 characters long'], 
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
+        minlength: [5, 'Email must be at least 5 characters long'],
+
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    socketId: {
+        type: String,
+        default: null,
+    },
+});
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign(
+        { userId: this._id },
+        jwt_token,
+        { expiresIn: '1h' }
+    );
+    return token;
+}
+userSchema.comparePassword = async function (candidatePassword) {
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    if (!isMatch) {
+        throw new Error('Invalid password');
+    }
+    return isMatch;
+}
+
+userSchema.statics.hashPassword = async function (password) {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+}
+
+const userModel = mongoose.model('User', userSchema);
+module.exports = userModel;
--- Backend/server.js
+++ Backend/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 
 const server = http.createServer(app);
+console.log("jwt token", process.env.JWT_SECRET); 
 
 server.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
