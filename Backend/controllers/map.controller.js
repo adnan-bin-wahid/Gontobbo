465b56f35475c20e01c06e8ab23b28067b22c169
sukanya1426 <rahmanmahdiya2002@gmail.com>
2025-07-06 02:38:54 +0600

    Add map route

---
+++ Backend/controllers/map.controller.js
@@ -0,0 +1,19 @@
+const mapService = require('../services/maps.service');
+const { validationResult } = require('express-validator');
+
+module.exports.getCoordinates = async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { address } = req.query;
+
+  try {
+    const coordinates = await mapService.getAddressCoordinates(address);
+    return res.status(200).json(coordinates);
+  } catch (error) {
+    console.error('Error fetching coordinates:', error);
+    return res.status(404).json({message: 'Coordinates not found'})
+  }
+}
